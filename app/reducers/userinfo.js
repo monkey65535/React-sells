41e8332ef0b97e8393fd9f479676063f9c904a9e
dilidili <466311467@qq.com>
2017-06-01 22:12:20 +0800

    init

--- app/reducers/userinfo.js
+++ app/reducers/userinfo.js
@@ -4,7 +4,7 @@ const initialState = {}
 
 export default function userinfo (state = initialState, action) {
     switch (action.type) {
-        case actionTypes.USERINFO_UPDATE:
+        case actionTypes.USERINFO_LOGIN:
             return action.data
         default:
             return state
