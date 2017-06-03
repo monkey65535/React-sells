187f52e5713442c28bdbd9ab41bafedc9ae09ed4
dilidili <466311467@qq.com>
2017-06-03 15:30:39 +0800

    change

--- README.md
+++ README.md
@@ -1,18 +1,3 @@
-# react-o2o
+# react-simple-o2o-demo
 
-react 打造大众点评 WebApp
-慕课网实战
-
-## 运行
-```
-npm start 运行webpack本地服务
-npm run mock 运行使用koa进行mooc的数据
-```
-
-
-## 常用知识
-### === 与 null
-在程序中，所有的判断尽量使用===，除了判断null的情况
-
-### 2.localstorage
-在IOS 和safari的无痕模式下，使用localStorage会报错
+代码尚在开发中
--- app/containers/index.jsx
+++ app/containers/index.jsx
@@ -1,38 +1,15 @@
-import React from 'react';
-import PureRenderMixin from 'react-addons-pure-render-mixin';
-
-import LocalStorage from '../util/localStore'; 
-import {CITYNAME} from '../Config/Config';
+import React from 'react'
+import PureRenderMixin from 'react-addons-pure-render-mixin'
 
 class App extends React.Component {
     constructor(props, context) {
         super(props, context);
-
         this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
-
-        this.state = {
-            initDone:false
-        }
-    }
-    componentDidMount(){
-
-        //从localStorage中获取城市
-        let cityName = LocalStorage.getItem(CITYNAME);
-        if(cityName == null){
-            cityName = '北京';
-        }
-        // TODO 将城市列表存储到redux中
-
-        setTimeout(()=> {
-            this.setState({
-                initDone:true
-            })
-        },1000)
     }
     render() {
         return (
             <div>
-                {this.state.initDone ? this.props.children : <div>加载中···</div>}
+                {this.props.children}
             </div>
         )
     }
