1866e8f3cd2bbd0723d0b71f84a70f7af139e626
dilidili <466311467@qq.com>
2017-06-05 22:07:28 +0800

    使用DOM方法实现上拉加载更多

--- app/components/LoadMore/LoadMore.jsx
+++ app/components/LoadMore/LoadMore.jsx
@@ -10,9 +10,35 @@ class LoadMore extends React.Component {
     LoadMoreHandle(){
         this.props.loadMoreFn();
     }
+    componentDidMount(){
+
+        const loadMoreFn = this.props.loadMoreFn;
+        let warpper = this.refs.warpper;
+        let winHeight = window.screen.height;
+
+        let timeOutId = null;
+        function callBack(){
+            const top = warpper.getBoundingClientRect().top;
+            if(top && top < winHeight){
+                loadMoreFn();
+            }
+        }
+
+        window.addEventListener('scroll',()=>{
+            // 如果isLoadingMore = true 那么不做处理
+            if(this.props.isLoadingMore) return;
+            if(timeOutId){
+                clearTimeout(timeOutId);
+            }
+
+            timeOutId = setTimeout(callBack,50);
+        },false);
+
+        
+    }
     render() {
         return (
-            <div className="load-more">
+            <div className="load-more" ref="warpper">
                 {this.props.isLoadingMore ? <span>加载中···</span> : <span onClick = {this.LoadMoreHandle.bind(this)}>加载更多</span>}
             </div>
         )
